e2dcbe785a6b01c4a5d76f3aaf5c492ef62bf812
StevenSaavedra <saavedras_1@carolinau.edu>
2025-02-16 16:29:14 -0500

    Add files via upload

---
+++ MidTerm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+int d1,d2,d3;
+
+printf("Please type a 3 digit number.\n");
+scanf("%1d%1d%1d", &d1,&d2,&d3);
+
+printf("Number reversed: %d%d%d\n", d3,d2,d1);
+
+return 0;
+
+}
