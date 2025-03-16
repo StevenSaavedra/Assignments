611afe893ed769527dab49202651777e796b7259
StevenSaavedra <saavedras_1@carolinau.edu>
2025-03-16 19:41:24 -0400

    Update Ass 2 Steven Saavedra.cpp

--- Ass 2 Steven Saavedra.cpp
+++ Ass 2 Steven Saavedra.cpp
@@ -15,8 +15,10 @@ double sum = 0;
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++) {
-            cout << a[i][j]<<" "<< endl;
+            cout << a[i][j]<<" ";
         }
+
+        cout << " " << endl;
 
     }
 
